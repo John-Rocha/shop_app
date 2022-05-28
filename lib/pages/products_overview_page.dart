db6fc3ac64389df13fe3eaade225332801939698
Johnathan Rocha <johnathanrocha@gmail.com>
2022-05-28 00:47:34 -0300

    feat: configure product item

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/data/dummy_data.dart';
 import 'package:shop_app/models/product.dart';
+import 'package:shop_app/widgets/product_item.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
   ProductsOverviewPage({Key? key}) : super(key: key);
@@ -12,19 +13,19 @@ class ProductsOverviewPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha Loja'),
+        centerTitle: true,
       ),
-      body: Padding(
+      body: GridView.builder(
+        itemCount: loadProdutcs.length,
         padding: const EdgeInsets.all(10.0),
-        child: GridView.builder(
-          itemCount: loadProdutcs.length,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-          ),
-          itemBuilder: (conext, index) => Text(loadProdutcs[index].title),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
         ),
+        itemBuilder: (conext, index) =>
+            ProductItem(product: loadProdutcs[index]),
       ),
     );
   }
---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import 'package:shop_app/models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  const ProductItem({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          title: Text(product.title),
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: const Icon(Icons.favorite),
+            color: Theme.of(context).colorScheme.secondary,
+            onPressed: () {},
+          ),
+          trailing: IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            color: Theme.of(context).colorScheme.secondary,
+            onPressed: () {},
+          ),
+        ),
+        child: Image.network(
+          product.imageUrl,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
