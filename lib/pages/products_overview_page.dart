604fa5bb5da5d75daa569f055239036bb1e682da
Johnathan Rocha <johnathanrocha@gmail.com>
2022-05-29 20:38:07 -0300

    feat: add favorites menu

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/product_list.dart';
 import 'package:shop_app/widgets/product_grid.dart';
+
+enum FilterOptions { favorite, all }
 
 class ProductsOverviewPage extends StatelessWidget {
   const ProductsOverviewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha Loja'),
         centerTitle: true,
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(Icons.more_horiz_outlined),
+            color: Theme.of(context).colorScheme.background,
+            itemBuilder: (context) => [
+              const PopupMenuItem(
+                value: FilterOptions.favorite,
+                child: Text('Somente Favoritas'),
+              ),
+              const PopupMenuItem(
+                value: FilterOptions.all,
+                child: Text('Todos'),
+              ),
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              if (selectedValue == FilterOptions.favorite) {
+                provider.showFavoriteOnly();
+              } else {
+                provider.showAll();
+              }
+            },
+          ),
+        ],
       ),
       body: const ProductGrid(),
       backgroundColor: Theme.of(context).colorScheme.background,
