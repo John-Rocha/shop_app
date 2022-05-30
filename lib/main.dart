7e1065e72bc9f56e1ce765401ef447fc9a04acad
Johnathan Rocha <johnathanrocha@gmail.com>
2022-05-30 19:45:09 -0300

    feat: add product detail page

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:shop_app/providers/product_provider.dart';
 import 'package:shop_app/utils/app_routes.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
