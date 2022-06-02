ac6c9c153d2e08c638a93b6ca74d28349e1fc491
Johnathan Rocha <johnathanrocha@gmail.com>
2022-06-02 14:13:11 -0300

    feat: create form for new product

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -1,15 +1,136 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-class ProductFormPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:shop_app/models/product.dart';
+
+class ProductFormPage extends StatefulWidget {
   const ProductFormPage({Key? key}) : super(key: key);
+
+  @override
+  State<ProductFormPage> createState() => _ProductFormPageState();
+}
+
+class _ProductFormPageState extends State<ProductFormPage> {
+  final _imageUrlFocus = FocusNode();
+  final _imageEC = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  final _formData = Map<String, dynamic>();
+
+  @override
+  void initState() {
+    super.initState();
+    _imageUrlFocus.addListener(updateImage);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _imageEC.dispose();
+    _imageUrlFocus.removeListener(updateImage);
+  }
+
+  void updateImage() {
+    setState(() {});
+  }
+
+  void _submitForm() {
+    _formKey.currentState?.save();
+    final newProduct = Product(
+      id: Random().nextDouble().toString(),
+      name: _formData['name'],
+      description: _formData['description'],
+      price: _formData['price'],
+      imageUrl: _formData['imageUrl'],
+    );
+
+    print(newProduct.id);
+    print(newProduct.name);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Formulário de Produto'),
+        actions: [
+          IconButton(
+            onPressed: _submitForm,
+            icon: const Icon(Icons.save),
+          ),
+        ],
       ),
-      body: Container(),
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Nome'),
+                textInputAction: TextInputAction.next,
+                onSaved: (name) => _formData['name'] = name ?? '',
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Preço'),
+                textInputAction: TextInputAction.next,
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+                onSaved: (price) =>
+                    _formData['price'] = double.tryParse(price ?? '0'),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Descrição'),
+                keyboardType: TextInputType.multiline,
+                maxLines: 3,
+                onSaved: (description) =>
+                    _formData['description'] = description ?? '',
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: _imageEC,
+                      decoration:
+                          const InputDecoration(labelText: 'URL da imagem'),
+                      keyboardType: TextInputType.url,
+                      textInputAction: TextInputAction.done,
+                      focusNode: _imageUrlFocus,
+                      onFieldSubmitted: (_) => _submitForm(),
+                      onSaved: (imageUrl) =>
+                          _formData['imageUrl'] = imageUrl ?? '',
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    margin: const EdgeInsets.only(
+                      top: 10,
+                      left: 10,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    alignment: Alignment.center,
+                    child: _imageEC.text.isEmpty
+                        ? const Text('Informe a URL')
+                        : FittedBox(
+                            child: Image.network(
+                              _imageEC.text,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
