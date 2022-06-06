2ec08d5bfda71560365774fc410540c6970525a4
Johnathan Rocha <johnathanrocha@gmail.com>
2022-06-06 20:15:41 -0300

    feat: load orders from Firebase

--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -4,8 +4,32 @@ import 'package:shop_app/providers/order_provider.dart';
 import 'package:shop_app/widgets/app_drawer.dart';
 import 'package:shop_app/widgets/order_widget.dart';
 
-class OrdersPage extends StatelessWidget {
+class OrdersPage extends StatefulWidget {
   const OrdersPage({Key? key}) : super(key: key);
+
+  @override
+  State<OrdersPage> createState() => _OrdersPageState();
+}
+
+class _OrdersPageState extends State<OrdersPage> {
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<OrderProvider>(context, listen: false)
+        .loadOrders()
+        .then((value) {
+      setState(() => _isLoading = false);
+    });
+  }
+
+  Future<void> _refreshOrders(BuildContext context) {
+    return Provider.of<OrderProvider>(
+      context,
+      listen: false,
+    ).loadOrders();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,11 +40,18 @@ class OrdersPage extends StatelessWidget {
       ),
       backgroundColor: Theme.of(context).colorScheme.background,
       drawer: const AppDrawer(),
-      body: ListView.builder(
-        itemCount: orders.itemsCount,
-        itemBuilder: (context, index) =>
-            OrderWidget(order: orders.items[index]),
-      ),
+      body: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : RefreshIndicator(
+              onRefresh: () => _refreshOrders(context),
+              child: ListView.builder(
+                itemCount: orders.itemsCount,
+                itemBuilder: (context, index) =>
+                    OrderWidget(order: orders.items[index]),
+              ),
+            ),
     );
   }
 }
--- lib/providers/order_provider.dart
+++ lib/providers/order_provider.dart
@@ -2,9 +2,11 @@ import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop_app/models/cart.dart';
 import 'package:shop_app/models/order.dart';
 import 'package:shop_app/providers/cart_provider.dart';
 import 'package:shop_app/utils/app_constants.dart';
+import 'package:shop_app/widgets/cart_item_widget.dart';
 
 class OrderProvider with ChangeNotifier {
   final List<Order> _items = [];
@@ -15,6 +17,33 @@ class OrderProvider with ChangeNotifier {
 
   int get itemsCount {
     return _items.length;
+  }
+
+  Future<void> loadOrders() async {
+    _items.clear();
+    final response =
+        await http.get(Uri.parse('${AppConstants.kOrderBaseUrl}.json'));
+    if (response.body == 'null') return;
+    Map<String, dynamic> data = jsonDecode(response.body);
+    data.forEach((orderId, orderData) {
+      _items.add(
+        Order(
+          id: orderId,
+          date: DateTime.parse(orderData['date']),
+          total: orderData['total'],
+          products: (orderData['products'] as List<dynamic>).map((item) {
+            return Cart(
+              id: item['id'],
+              productId: item['productId'],
+              name: item['name'],
+              quantity: item['quantity'],
+              price: item['price'],
+            );
+          }).toList(),
+        ),
+      );
+    });
+    notifyListeners();
   }
 
   Future<void> addOrder(CartProvider cart) async {
