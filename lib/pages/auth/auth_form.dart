c6a4ebf4343cccb1fd22da5a2eb31a374c1570fd
Johnathan Rocha <johnathanrocha@gmail.com>
2022-06-11 00:19:56 -0300

    feat: add animation in auth form

--- lib/pages/auth/auth_form.dart
+++ lib/pages/auth/auth_form.dart
@@ -15,7 +15,8 @@ class AuthForm extends StatefulWidget {
   State<AuthForm> createState() => _AuthFormState();
 }
 
-class _AuthFormState extends State<AuthForm> {
+class _AuthFormState extends State<AuthForm>
+    with SingleTickerProviderStateMixin {
   final _passwordEC = TextEditingController();
   final _emailForgotPasswordEC = TextEditingController();
   final _formKey = GlobalKey<FormState>();
@@ -23,10 +24,39 @@ class _AuthFormState extends State<AuthForm> {
   AuthMode _authMode = AuthMode.login;
   FirebaseAuth firebaseAuth = FirebaseAuth.instance;
 
+  AnimationController? _controller;
+  Animation<double>? _opacityAnimation;
+
   Map<String, String> _authData = {
     'email': '',
     'password': '',
   };
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+    _opacityAnimation = Tween(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller!,
+        curve: Curves.linear,
+      ),
+    );
+
+    // _heigtAnimation?.addListener(() => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller?.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,44 +67,55 @@ class _AuthFormState extends State<AuthForm> {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10),
       ),
-      child: Container(
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeIn,
         padding: const EdgeInsets.all(16),
         height: _isLogin() ? 360 : 400,
+        // height: _heigtAnimation?.value.height ?? (_isLogin() ? 360 : 400),
         width: deviceSize.width * 0.75,
         child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'E-mail'),
-                  keyboardType: TextInputType.emailAddress,
-                  onSaved: (email) => _authData['email'] = email ?? '',
-                  validator: (emailValue) {
-                    final email = emailValue ?? '';
-                    if (email.trim().isEmpty || !email.contains("@")) {
-                      return 'Informe um e-mail válido';
-                    }
-                    return null;
-                  },
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'E-mail'),
+                keyboardType: TextInputType.emailAddress,
+                onSaved: (email) => _authData['email'] = email ?? '',
+                validator: (emailValue) {
+                  final email = emailValue ?? '';
+                  if (email.trim().isEmpty || !email.contains("@")) {
+                    return 'Informe um e-mail válido';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _passwordEC,
+                decoration: const InputDecoration(labelText: 'Senha'),
+                obscureText: true,
+                onSaved: (password) => _authData['password'] = password ?? '',
+                validator: (passwordValue) {
+                  final password = passwordValue ?? '';
+                  if (password.isEmpty) {
+                    return 'Informe uma senha válida';
+                  }
+                  if (password.length < 5) {
+                    return 'A senha precisa conter no mínimo 6 dígitos';
+                  }
+                  return null;
+                },
+              ),
+              AnimatedContainer(
+                constraints: BoxConstraints(
+                  minHeight: _isLogin() ? 0 : 60,
+                  maxHeight: _isLogin() ? 0 : 120,
                 ),
-                TextFormField(
-                  controller: _passwordEC,
-                  decoration: const InputDecoration(labelText: 'Senha'),
-                  obscureText: true,
-                  onSaved: (password) => _authData['password'] = password ?? '',
-                  validator: (passwordValue) {
-                    final password = passwordValue ?? '';
-                    if (password.isEmpty) {
-                      return 'Informe uma senha válida';
-                    }
-                    if (password.length < 5) {
-                      return 'A senha precisa conter no mínimo 6 dígitos';
-                    }
-                    return null;
-                  },
-                ),
-                if (_isSignup())
-                  TextFormField(
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.linear,
+                child: FadeTransition(
+                  opacity: _opacityAnimation!,
+                  child: TextFormField(
                     decoration:
                         const InputDecoration(labelText: 'Confirmar Senha'),
                     obscureText: true,
@@ -88,39 +129,41 @@ class _AuthFormState extends State<AuthForm> {
                             return null;
                           },
                   ),
-                const SizedBox(height: 20),
-                _isLoading
-                    ? const CircularProgressIndicator()
-                    : ElevatedButton(
-                        onPressed: _submit,
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 30,
-                            vertical: 8,
-                          ),
-                        ),
-                        child: _isLogin()
-                            ? Text('Entrar'.toUpperCase())
-                            : Text('Registrar'.toUpperCase()),
-                      ),
-                const Spacer(),
-                TextButton(
-                  onPressed: _switchAuthMode,
-                  child: Text(
-                    _isLogin()
-                        ? 'Deseja Registrar?'.toUpperCase()
-                        : 'Já possui conta?'.toUpperCase(),
-                  ),
                 ),
-                TextButton(
-                  onPressed: () => _forgotPassword(context),
-                  child: const Text('Esqueceu sua senha?'),
-                )
-              ],
-            )),
+              ),
+              const SizedBox(height: 20),
+              _isLoading
+                  ? const CircularProgressIndicator()
+                  : ElevatedButton(
+                      onPressed: _submit,
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 30,
+                          vertical: 8,
+                        ),
+                      ),
+                      child: _isLogin()
+                          ? Text('Entrar'.toUpperCase())
+                          : Text('Registrar'.toUpperCase()),
+                    ),
+              TextButton(
+                onPressed: _switchAuthMode,
+                child: Text(
+                  _isLogin()
+                      ? 'Deseja Registrar?'.toUpperCase()
+                      : 'Já possui conta?'.toUpperCase(),
+                ),
+              ),
+              TextButton(
+                onPressed: () => _forgotPassword(context),
+                child: const Text('Esqueceu sua senha?'),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -149,7 +192,7 @@ class _AuthFormState extends State<AuthForm> {
                   email: _emailForgotPasswordEC.text.trim(),
                 );
               } on FirebaseAuthException catch (e) {
-                print('O erro $e');
+                _showErrorDialog(e.toString());
               } finally {
                 Navigator.of(context).pop();
               }
@@ -165,8 +208,10 @@ class _AuthFormState extends State<AuthForm> {
     setState(() {
       if (_isLogin()) {
         _authMode = AuthMode.signup;
+        _controller?.forward();
       } else {
         _authMode = AuthMode.login;
+        _controller?.reverse();
       }
     });
   }
@@ -208,7 +253,7 @@ class _AuthFormState extends State<AuthForm> {
   }
 
   bool _isLogin() => _authMode == AuthMode.login;
-  bool _isSignup() => _authMode == AuthMode.signup;
+  // bool _isSignup() => _authMode == AuthMode.signup;
 
   void _showErrorDialog(String message) {
     showDialog(
